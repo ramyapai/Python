a31f61e587e2498a2af6e2d2d6aedfe853a256bd
ramyapai <ramyarpai@gmail.com>
2023-04-18 16:13:31 +0530

    Commit

--- greatestOfThreeNumbers.py
+++
@@ -1,9 +0,0 @@
-a,b,c = map(int, input("Please enter three integers: ").split())
-
-if a>b == True and a>c ==True:
-    print('{a} is greater'.format(a=a))
-elif b>a == True and b>c == True:
-    print('{b} is grater'.format(b=b))
-else:
-    print('{c} is greater'.format(c=c))
-    
